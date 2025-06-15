9f0d9787c57285fc42152e68a5d2bc30a2a08c0f
Joao Pedro Fontes <joaopedrofonttes@gmail.com>
2025-06-15 10:25:44 -0300

    novos gráficos para tarefa 14

--- tarefa_14/graf_1.py
+++ tarefa_14/graf_1.py
@@ -1,12 +1,31 @@
 import matplotlib.pyplot as plt
-from data import sizes_bytes, tempos
+
+sizes = [
+    8, 16, 32, 64, 128, 256, 512, 1024, 2048,
+    4096, 8192, 16384, 32768, 65536,
+    131072, 262144, 524288, 1048576,
+    2097152, 4194304, 8388608, 16777216
+]
+
+times = [
+    0.34, 0.28, 0.29, 0.30, 0.31, 0.33, 0.38, 0.49, 1.19,
+    1.99, 2.22, 2.62, 3.45, 1.54,
+    3.82, 4.89, 7.03, 8.82,
+    15.35, 26.81, 47.82, 96.01
+]
+
+sizes_kb_labels = [f"{s//1024 if s >= 1024 else s} {'KB' if s >= 1024 else 'B'}" for s in sizes]
 
 
-plt.figure(figsize=(10, 6))
-plt.semilogx(sizes_bytes, tempos, marker='o')
-plt.title('Tempo vs Tamanho (Eixo X Logarítmico)')
-plt.xlabel('Tamanho da Mensagem (Bytes, escala log)')
-plt.ylabel('Tempo (s)')
-plt.grid(True, which="both", ls="--")
+plt.figure(figsize=(12, 6))
+plt.plot(sizes_kb_labels, times, marker='o', linestyle='-', color='blue')
+plt.xticks(rotation=45)
+plt.xlabel("Tamanho da Mensagem")
+plt.ylabel("Tempo de Execução")
+plt.title("Tempo de Execução vs Tamanho da Mensagem (MPI)")
+plt.grid(True)
 plt.tight_layout()
+
+#plt.savefig("grafico_1.png", dpi=300)
+
 plt.show()
--- tarefa_14/graf_2.py
+++ tarefa_14/graf_2.py
@@ -1,11 +1,32 @@
 import matplotlib.pyplot as plt
-from data import sizes_bytes, tempos
 
-plt.figure(figsize=(10, 6))
-plt.loglog(sizes_bytes, tempos, marker='o')
-plt.title('Tempo vs Tamanho (Escala Log-Log)')
-plt.xlabel('Tamanho da Mensagem (Bytes)')
-plt.ylabel('Tempo (s)')
-plt.grid(True, which="both", ls="--")
+
+sizes_2 = [
+    8, 16, 32, 64, 128, 256, 512, 1024, 2048,
+    4096, 8192, 16384, 32768, 65536,
+    131072, 262144, 524288, 1048576,
+    2097152, 4194304, 8388608, 16777216
+]
+
+
+times_2 = [
+    0.03, 0.03, 0.04, 0.04, 0.04, 0.05, 0.05, 0.06, 0.07,
+    0.09, 0.10, 0.15, 0.21, 0.33,
+    0.59, 0.56, 0.99, 1.84,
+    3.54, 6.94, 13.76, 27.39
+]
+
+sizes_kb_labels_2 = [f"{s//1024 if s >= 1024 else s} {'KB' if s >= 1024 else 'B'}" for s in sizes_2]
+
+plt.figure(figsize=(12, 6))
+plt.plot(sizes_kb_labels_2, times_2, marker='o', linestyle='-', color='green')
+plt.xticks(rotation=45)
+plt.xlabel("Tamanho da Mensagem")
+plt.ylabel("Tempo de Execução")
+plt.title("Tempo de Execução vs Tamanho da Mensagem (MPI) - Segunda Medição")
+plt.grid(True)
 plt.tight_layout()
+
+#plt.savefig("grafico_2.png", dpi=300)
+
 plt.show()
